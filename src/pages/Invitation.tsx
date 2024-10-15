4739625b267d39e7219e07255b9b7d47a0aebb20
최준호 <junho5336@gmail.com>
2024-10-15 15:00:10 +0900

    refactor: 로그인 상태 확인 로직 추가

--- src/pages/Invitation.tsx
+++ src/pages/Invitation.tsx
@@ -1,6 +1,8 @@
 import { Button, Container } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useJoinGroup } from 'api/InvitationApi';
+import { useGetGroups } from 'api/StudyApi';
+import { useEffect } from 'react';
 
 function Invitation() {
   const navigate = useNavigate();
@@ -20,9 +22,23 @@ function Invitation() {
         });
       }
     } catch (err) {
-      throw new Error('참여 과정에서 문제가 발생했습니다');
+      alert('참여 과정에서 문제가 발생했습니다. 관리자에게 문의해주세요!');
     }
   };
+
+  const fetchGroups = async () => {
+    try {
+      await useGetGroups();
+    } catch (err) {
+      console.log(err);
+      alert('로그인 후 다시 시도해주세요');
+      navigate('/signin');
+    }
+  };
+
+  useEffect(() => {
+    fetchGroups();
+  }, []);
 
   return (
     <Container maxWidth="md">
